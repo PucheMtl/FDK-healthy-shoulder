b181f277c3038ccbcaac771defa58f948ee8e2e6
Margaux <margaux.peixoto.1@ens.etsmtl.com>
2023-05-24 14:11:02 +0200

    scapula morphing
    morphing du scapula générique de Anybody au scapula moyen issue du modèle de Leen. 
    Utilisation des fonction Affine, RBF, STL transform.

---
+++ scapula_stl_test1/Scaling_Marta example/SourceHumerusLeftLocal_picked_points.pp
@@ -0,0 +1,52 @@
+<!DOCTYPE PickedPoints>
+<PickedPoints>
+ <DocumentData>
+  <DateTime date="2019-01-08" time="14:27:24"/>
+  <User name="Marta"/>
+  <DataFileName name="SourceHumerusLeftLocal.stl"/>
+  <templateName name="new Template"/>
+ </DocumentData>
+ <point y="-0.133271" x="-0.0114373" active="1" z="0.0191031" name="DT"/>
+ <point y="-0.00108594" x="-0.0173168" active="1" z="-0.0216969" name="LT1"/>
+ <point y="-0.00980318" x="-0.00250399" active="1" z="-0.0291751" name="LT2"/>
+ <point y="-0.0258922" x="-0.00195204" active="1" z="-0.0206435" name="SN"/>
+ <point y="-0.0134596" x="0.0207789" active="1" z="-0.0150297" name="AN1"/>
+ <point y="0.00858506" x="-0.0266883" active="1" z="-0.00688094" name="GT1"/>
+ <point y="0.0129324" x="-0.0261131" active="1" z="0.00739211" name="GT2"/>
+ <point y="0.0122679" x="-0.000470361" active="1" z="0.0228402" name="GT3"/>
+ <point y="-0.00250789" x="0.0223008" active="1" z="-0.0213358" name="AN2"/>
+ <point y="0.0143036" x="0.0111209" active="1" z="-0.0216688" name="AN3"/>
+ <point y="0.0228955" x="0.0115199" active="1" z="-0.00782876" name="AN4"/>
+ <point y="0.01976" x="0.00329098" active="1" z="0.0166529" name="AN5"/>
+ <point y="0.00848361" x="0.00999215" active="1" z="0.0234921" name="AN6"/>
+ <point y="0.00452372" x="0.0281171" active="1" z="0.00949638" name="AN7"/>
+ <point y="0.0125881" x="0.0230306" active="1" z="-0.0140014" name="GHC"/>
+ <point y="-0.0744422" x="0.00830401" active="1" z="-0.012981" name="MLR3"/>
+ <point y="-0.21129" x="0.0115724" active="1" z="0.00275289" name="MLR2"/>
+ <point y="-0.215028" x="-0.00677984" active="1" z="0.0148903" name="LSR2"/>
+ <point y="-0.0723123" x="-0.0177545" active="1" z="0.00499548" name="LSR3"/>
+ <point y="-0.00208537" x="-0.00387394" active="1" z="0.0236599" name="GT4"/>
+ <point y="0.0225168" x="-0.00882285" active="1" z="0.00880945" name="GT5"/>
+ <point y="-0.0122622" x="0.0282937" active="1" z="0.00880252" name="GT6"/>
+ <point y="-0.337262" x="-0.0328731" active="1" z="0.0022738" name="EpicL"/>
+ <point y="-0.342759" x="0.0345741" active="1" z="-0.000126752" name="EpicM"/>
+ <point y="-0.328764" x="0.0314794" active="1" z="0.00191699" name="EL"/>
+ <point y="-0.347967" x="0.0244" active="1" z="-0.0110458" name="Cap"/>
+ <point y="-0.348511" x="0.0125848" active="1" z="-0.00904438" name="Tr1"/>
+ <point y="-0.349295" x="0.00563509" active="1" z="-0.0122113" name="Tr2"/>
+ <point y="-0.349896" x="0.00256144" active="1" z="-0.0131989" name="Tr3"/>
+ <point y="-0.353689" x="-0.0095633" active="1" z="-0.0144515" name="Tr4"/>
+ <point y="-0.354456" x="-0.0170794" active="1" z="-0.0155204" name="Tr5"/>
+ <point y="-0.336882" x="-0.0363204" active="1" z="-0.00861389" name="EM"/>
+ <point y="-0.329656" x="-0.006909" active="1" z="-0.0142331" name="CF1"/>
+ <point y="-0.330168" x="-0.0223261" active="1" z="-0.0156935" name="CF2"/>
+ <point y="-0.328144" x="0.0109095" active="1" z="-0.0114111" name="RF"/>
+ <point y="-0.298234" x="0.0209277" active="1" z="0.00726014" name="LSR1"/>
+ <point y="-0.302949" x="-0.0192253" active="1" z="-0.00369856" name="MLR1"/>
+ <point y="-0.352566" x="-0.0174175" active="1" z="-0.00143355" name="Tr5_1"/>
+ <point y="-0.346538" x="-0.00259712" active="1" z="-0.00121583" name="Tr3_1"/>
+ <point y="-0.343881" x="0.0132885" active="1" z="0.00548525" name="Tr1_1"/>
+ <point y="-0.326359" x="-0.00161799" active="1" z="-0.00326184" name="OF"/>
+ <point y="-0.341242" x="-0.0350145" active="1" z="-0.00446986" name="EM1"/>
+ <point y="-0.142694" x="-0.00988512" active="1" z="-0.00729757" name="MLR4"/>
+</PickedPoints>
---
+++ scapula_stl_test1/Scaling_Marta example/SourceHumerusLeftLocal_picked_points_Affine.pp
@@ -0,0 +1,29 @@
+<!DOCTYPE PickedPoints>
+<PickedPoints>
+ <DocumentData>
+  <DateTime time="11:24:02" date="2018-11-13"/>
+  <User name="Marta"/>
+  <DataFileName name="SourceHumerusLeftLocal.stl"/>
+  <templateName name="new Template"/>
+ </DocumentData>
+ <point active="0" y="-0.133271" name="DT" z="0.0191031" x="-0.0114373"/>
+ <point active="1" y="-0.00108594" name="LT1" z="-0.0216969" x="-0.0173168"/>
+ <point active="1" y="-0.00980318" name="LT2" z="-0.0291751" x="-0.00250399"/>
+ <point active="0" y="-0.0258922" name="SN" z="-0.0206435" x="-0.00195204"/>
+ <point active="0" y="-0.0134596" name="AN1" z="-0.0150297" x="0.0207789"/>
+ <point active="0" y="0.00858506" name="GT1" z="-0.00688094" x="-0.0266883"/>
+ <point active="0" y="0.0129324" name="GT2" z="0.00739211" x="-0.0261131"/>
+ <point active="0" y="0.0122679" name="GT3" z="0.0228402" x="-0.000470361"/>
+ <point active="0" y="-0.00250789" name="AN2" z="-0.0213358" x="0.0223008"/>
+ <point active="0" y="0.0143036" name="AN3" z="-0.0216688" x="0.0111209"/>
+ <point active="0" y="0.0228955" name="AN4" z="-0.00782876" x="0.0115199"/>
+ <point active="0" y="0.01976" name="AN5" z="0.0166529" x="0.00329098"/>
+ <point active="0" y="0.00848361" name="AN6" z="0.0234921" x="0.00999215"/>
+ <point active="0" y="0.00452372" name="AN7" z="0.00949638" x="0.0281171"/>
+ <point active="0" y="0.0125881" name="GHC" z="-0.0140014" x="0.0230306"/>
+ <point active="1" y="-0.0744422" name="MLR3" z="-0.012981" x="0.00830401"/>
+ <point active="1" y="-0.21129" name="MLR2" z="0.00275289" x="0.0115724"/>
+ <point active="1" y="-0.215028" name="LSR2" z="0.0148903" x="-0.00677984"/>
+ <point active="1" y="-0.0723123" name="LSR3" z="0.00499548" x="-0.0177545"/>
+ <point active="1" y="-0.00208537" name="GT4" z="0.0236599" x="-0.00387394"/>
+</PickedPoints>
